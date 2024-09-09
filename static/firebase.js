53f1e0af22d902ac8abd509b5d9e7c151f1861b3
pat <158650319+pnestrella@users.noreply.github.com>
2024-09-08 20:30:03 -0700

    Update firebase.js
    Browser session persistence

--- static/firebase.js
+++ static/firebase.js
@@ -140,7 +140,7 @@ LsubmitBTN.addEventListener('click', (e)=>{
 
 
 
-setPersistence(auth, inMemoryPersistence)
+setPersistence(auth, browserSessionPersistence)
   .then(() => {
     // Existing and future Auth states are now persisted in the current
     // session only. Closing the window would clear any existing state even
